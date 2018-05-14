e1daa038e8bfe2cc0a64f19a87233999ec804ba9
佐藤大介 <workshift56@gmail.com>
2018-05-14 23:52:05 +0900

    Tweetモデルの作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,17 @@
 class TweetsController < ApplicationController
 
   def index
-    
+    @tweet = Tweet.new
   end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.save
+    redirect_to root_path
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:body)
+    end
 end
---
+++ db/migrate/20180514143112_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tweets do |t|
+      t.string :body
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
