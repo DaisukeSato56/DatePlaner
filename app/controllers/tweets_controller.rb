875d11873779febca2df5491b97954d61f3307c0
佐藤大介 <workshift56@gmail.com>
2018-05-16 19:07:21 +0900

    googleplacesAPIを用いて、デート場所を取得

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,8 @@
+require 'net/http'
+require 'uri'
+require 'pry'
+require 'json'
+
 class TweetsController < ApplicationController
 
   before_action :set_data, only: [:destroy]
@@ -7,17 +12,39 @@ class TweetsController < ApplicationController
     @tweets = Tweet.all
   end
 
-  def asset
-  end
-
   def create
-    # params[:input]の内容をジオコーダーで取得し、緯度経度情報の取得
     # 緯度経度情報を元にgoogleplacesAPIを用いて、周辺の施設を検索
     # 施設情報の中からランダムで１つ抽出し、並べる
     @tweet = Tweet.new(tweet_params)
     @tweet.save
+
+    googleplacesAPI = ENV['googlePlacesAPIKey']
+
+    lat = @tweet.latitude
+    lng = @tweet.longitude
+    rad = 2000
+    language = 'ja'
+
+    places = [ ["restaurant"], ["aquarium", "park", "museum", "shopping_mall"], ["cafe"] ]
+
+    result_places = []
+    places.each do |place|
+      types = place[rand(place.length)]
+
+      url = URI.parse(URI.escape("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&radius=#{rad}&types=#{types}&sensor=false&language=ja&key=#{googleplacesAPI}"))
+      res = Net::HTTP.start(url.host, url.port, use_ssl: true) { |http|
+        http.get(url.path + '?' + url.query);
+      }
+
+      obj = JSON.parse(res.body)
+      result_places << obj["results"][rand(obj["results"].length)]["name"]
+    end
+
+    # objのresultsキーの0番目の配列の要素の中のnameキーを取得する
+    # obj["results"][0]["name"]
+
     respond_to do |format|
-      format.html { redirect_to controller: 'tweets', action: 'index', notice: "投稿しました" }
+      format.html { redirect_to controller: 'tweets', action: 'index' }
       format.json
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
    root 'tweets#index'
    resources :tweets, only: [:create, :index, :destroy]
-   get '/asset' => 'tweets#asset'
 end
